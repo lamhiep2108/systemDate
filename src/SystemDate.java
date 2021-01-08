d44cc30e57b62e595496b2ee1f44ac9fa0dceb65
Admin <hiepnothip@gmail.com>
2021-01-08 14:57:08 +0700

    k

---
+++ src/SystemDate.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class SystemDate {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("date new"+ now);
+    }
+}
